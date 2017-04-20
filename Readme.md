2a5f81de968b1f6483104aa9e8e06c0a2fd49e9a
Alexander Egorichev <alexchev92@gmail.com>
2017-04-20 17:40:25 +0300

    Made html and images

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Личный проект «Седона» [![Build status][travis-image]][travis-url] [![Dependency status][dependency-image]][dependency-url]
+
+* Автор: Егоричев Александр
+
+## Сервер для разработки
+
+Для запуска лайв-сервера нужен [Node.js](https://nodejs.org) версии 0.12 или выше.
+
+### Установка
+
+```bash
+$ git clone https://github.com/sedona.git
+$ cd sedona
+$ npm i
+```
+
+### Запуск
+
+```bash
+$ npm start
+```
+
+После выполнения команды, откройте в браузере `http://127.0.0.1:3000`.
+
+### Тестирование
+
+```bash
+$ npm test
+```
+
---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+'use strict';
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let plumber = require('gulp-plumber');
+let postcss = require('gulp-postcss');
+let autoprefixer = require('autoprefixer');
+let server = require('browser-sync');
+let svgmin = require('gulp-svgmin');
+let cheerio = require('gulp-cheerio');
+let replace = require('gulp-replace');
+let svgSprite = require('gulp-svg-sprite');
+let sassGlob = require('gulp-sass-glob');
+let cssUnit = require('gulp-css-unit');
+let concatCss = require('gulp-concat-css');
+let minify = require("gulp-csso");
+
+gulp.task('css:foundation', function() {
+  return gulp.src('./node_modules/normalize.css/normalize.css')
+      .pipe(concatCss('foundation.css'))
+      .pipe(minify())
+      .pipe(gulp.dest('css/'))
+});
+
+gulp.task('sprite:svg', function() {
+  return gulp.src('./img/icons/*.svg')
+      .pipe(svgmin({
+        js2svg: {
+          pretty: true
+        }
+      }))
+      .pipe(cheerio({
+        run: function($) {
+          $('[fill]').removeAttr('fill');
+          $('[stroke]').removeAttr('stroke');
+          $('[style]').removeAttr('style');
+        },
+        parserOptions: {xmlMode: true}
+      }))
+      .pipe(replace('&gt;', '>'))
+      .pipe(svgSprite({
+        mode: {
+          symbol: {
+            sprite: '../sprite.svg'
+          }
+        }
+      }))
+      .pipe(gulp.dest('./img/'));
+});
+
+gulp.task('style', function() {
+  gulp.src('sass/style.scss')
+    .pipe(plumber())
+	.pipe(sassGlob())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        'last 1 version',
+        'last 2 Chrome versions',
+        'last 2 Firefox versions',
+        'last 2 Opera versions',
+        'last 2 Edge versions'
+      ]})
+    ]))
+	.pipe(cssUnit({type: 'px-to-rem', rootSize: 16}))
+    .pipe(gulp.dest('css'))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task('serve', ['style'], function() {
+  server.init({
+    server: '.',
+    notify: false,
+    open: true,
+    ui: false
+  });
+
+  gulp.watch('sass/**/*.{scss,sass}', ['style']);
+  gulp.watch('*.html').on('change', server.reload);
+});
