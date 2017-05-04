6249ac573b40dcf73944784ac9a8d9b4ec581462
Alexander Egorichev <alexchev92@gmail.com>
2017-05-04 16:37:36 +0300

    no-js for mobile menu

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 let toggleMenu = document.querySelector('.page-header__toggle-menu');
 let menu = document.querySelector('.main-nav');
 
+menu.classList.remove('no-js');
+
 toggleMenu.addEventListener('click', function(e) {
   e.preventDefault();
   this.classList.toggle('active');
